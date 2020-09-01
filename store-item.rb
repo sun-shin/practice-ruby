73f6d2a9c1edbd185f0c3cd5072e14aa35299256
sun-shin <sunhanshin2@EMAIL.com>
2020-09-01 15:11:38 -0500

    added ruby syntax of hashes

--- store-item.rb
+++ store-item.rb
@@ -1,3 +1,7 @@
-banana = {color: yellow, price: 1.00, organic: no}
-apple = {color: red, price: 0.50, organic: yes}
-blueberry = {color: blue, price: 0.25, organic: yes}
+# banana = {color: yellow, price: 1.00, organic: no}
+# apple = {color: red, price: 0.50, organic: yes}
+# blueberry = {color: blue, price: 0.25, organic: yes}
+
+banana = {"color" => yellow, "price" => 1.00, "organic" => no}
+apple = {"color" => red, "price" => 0.50, "organic" => yes}
+blueberry = {"color" => blue, "price" => 0.25, "organic" => yes}
